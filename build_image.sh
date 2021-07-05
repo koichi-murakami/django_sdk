cb79ae2f5cdd734133d2a24560b782334859b2e9
Koichi Murakami <koichi.murakami@kek.jp>
2021-07-05 19:02:28 +0900

    change image name

--- build_image.sh
+++ build_image.sh
@@ -6,7 +6,7 @@ export DOCKER_BUILDKIT=1
 
 echo "@@ Build Docker image ..."
 
-target=koichimurakamik6/django_sdk:3.9.6
+target=koichimurakamik6/django_sdk:latest
 
 id=`docker image ls -q $target`
 
